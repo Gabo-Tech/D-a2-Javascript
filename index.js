af6265ff548a765651a03b1618c1c5442b5c067a
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-04-12 13:26:31 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,88 @@
+
+booleano1 = true;
+booleano2 = false;
+
+const booleanoAnd = booleano1 && booleano2;
+const booleanoOr = booleano1 || booleano2;
+const booleanoNot = booleana != booleano1;
+const booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!= booleano1 && != booleano2);
+
+const valorDivisión = (17/3).toFixed(2);
+const valorResto = 17%7;
+
+const A = 9;
+const B = "9";
+console.log(A==B);
+¿qué pasa si en vez de tener A == B, comprobamos que A === B? Que da false por que son formatos diferentes. 
+const mochila = [ele1, ele2, ele3, ele4, ele5, ele6, ele7, ele8, ele9, ele10];
+
+if (mochila.length > 10){
+    return " No puedo cargar con tantas cosas";
+} else if (mochila.length > 2 && mochila.length < 10){
+    return "Que bien voy con mi mochila";
+} else {
+    return "Creo que no necesito otra mochila";
+}
+const contarHasta10 = 0 
+
+for (var=i,contarHasta10>=10,i++){
+    contarHasta10 += 1;
+}.
+
+const diaFestivo = true 
+console.log(diaFestivo ? "Hoy no trabajo" : "Hoy trabajo");
+
+
+const arrayBucle = [];
+for(var i = 4,arrayBucle[arrayBucle-1]>=18, i++ ){
+    arrayBucle.push(i);
+}
+console.log(arrayBucle.reduce)
+const sum = arrayBucle.reduce();
+
+
+const array = [‘Con’, ‘Sofia’, ‘aprendiendo’, ‘bucles’];
+
+for (variable of array) { 
+    console.log(variable);
+}
+for (variable in array) { 
+    console.log(variable);
+}
+
+while (i < 20) {
+  if (i%3==0){
+      console.log("Patata");
+  }
+  i++;
+}
+switch(nota) {
+  case 0 - 4:
+    console.log("Insuficiente");
+    break;
+  case 5 - 6:
+    console.log("Suficiente");
+    break;
+  case 7 - 8:
+   console.log("Notable");
+   break;
+  case 9 - 10:
+   console.log("Sobresaliente");
+   break;
+  default:
+    console.log("Te salíste wey.");
+}
+
+function resta (a - b){
+    return a - b;
+}
+const multiply2 => typeof num == number ? num * 2 : "Debo ser ejecutada con un número";
+
+
+function multiply22(num){
+    if (typeof num == number){
+        return num * 2;
+    } else {
+        return "Debo ser ejecutada con un número";
+    }
+}
